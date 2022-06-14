355549cd28c04796b4d401c0f3015f883eb54816
hmrani <hicham.mrani@outlook.fr>
2022-06-14 16:26:18 +0200

    topCitiesFrance scraper

--- spiders/spiders/spiders/topCitiesFrance.py
+++ spiders/spiders/spiders/topCitiesFrance.py
@@ -3,7 +3,10 @@ import logging
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-JSON_FOLDER = '../../../json/'
+DOCS_FOLDER = '../../../docs/'
+
+# Name of the file where the results will be saved
+filename = 'topCitiesFrance.json'
 
 class TopcitiesfranceSpider(scrapy.Spider):
     name = 'topCitiesFrance'
@@ -11,34 +14,27 @@ class TopcitiesfranceSpider(scrapy.Spider):
     start_urls = ['https://one-week-in.com/35-cities-to-visit-in-france/']
 
     def parse(self, response):
+        ## we get the first <ol> element then text inside all <li> child
         cities = response.xpath("(//ol)[1]//li/a//text()").getall()
         yield {
             'cities': cities
         }
-
-# Name of the file where the results will be saved
-filename = 'topCitiesFrance.json'
-
 '''
 If file already exists, delete it before crawling (because Scrapy will 
 concatenate the last and new results otherwise)
 '''
 
-if filename in os.listdir(JSON_FOLDER):
-        os.remove(JSON_FOLDER + filename)
+if filename in os.listdir(DOCS_FOLDER):
+        os.remove(DOCS_FOLDER + filename)
 
 process = CrawlerProcess(settings = {
     'USER_AGENT': 'Chrome/97.0',
     'LOG_LEVEL': logging.INFO,
     'FEED_EXPORT_ENCODING': 'utf-8',
     "FEEDS": {
-        JSON_FOLDER + filename : {"format": "json"},
+        DOCS_FOLDER + filename : {"format": "json"},
     }
 })
 
 process.crawl(TopcitiesfranceSpider)
 process.start()
-
-with open(JSON_FOLDER + filename) as file:
-    cities = json.load(file)
-file.close()
