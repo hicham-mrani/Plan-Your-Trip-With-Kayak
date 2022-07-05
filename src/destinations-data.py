321b0a4e66c7f691386c111d0fe92fe7387cd657
hmrani <hicham.mrani@outlook.fr>
2022-07-06 00:06:11 +0200

    add cities polygons infos

--- src/destinations-data.py
+++ src/destinations-data.py
@@ -1,46 +1,50 @@
 import requests as req
+import os
 import json
 import pandas as pd
 import numpy as np
 from pathlib import Path
 
 #personnal lib
-from my_functions import export_csv
+from my_functions import export_csv, export_json
 
 # CONSTANTS
 DOCS_FOLDER = '../docs/'
+POLYGONS = '../docs/polygons/'
 FILENAME = 'topCitiesFrance.json'
 ENDPOINT =  'https://nominatim.openstreetmap.org/'
 SEARCH = ENDPOINT + 'search'
 
 def get_cities_infos(cities:list)->list:
     cities_infos = []
+    polygons = []
     if cities:
         for city in cities:
             try:
                 # we test if we recover the result at index 0 of our answer for the city request
-                response = req.get(SEARCH, params={'city': city, 'country':'france', 'format':'json'}).json()[0]
+                response = req.get(SEARCH, params={'city': city, 'country':'france', 'format':'json', 'polygon_geojson': 1}).json()[0]
             except IndexError: 
                 # if we have an exception, we test if we recover the result at index 0 of our answer for the street request
                 try:
-                    response = req.get(SEARCH, params={'street': city, 'country':'france', 'format':'json'}).json()[0]
+                    response = req.get(SEARCH, params={'street': city, 'country':'france', 'format':'json', 'polygon_geojson': 1}).json()[0]
                 except IndexError:
                     # if we have an exception again, so we pass and set the city gps as ""
                     response = False
                     pass
             if response:
+                city_name = response["display_name"].split(',')[0]
+
                 cities_infos.append({
-                    "city_name": response["display_name"].split(',')[0],
+                    "city_name": city_name,
                     "lat": response["lat"],
                     "lon": response["lon"],
-                    "boundingbox": response["boundingbox"]
                     })
+                export_json(dictionary=response["geojson"], rel_path=os.path.join(POLYGONS,'france/'), file_name = city_name)
             else:
                 cities_infos.append({
                     "city_name": city,
                     "lat": "",
                     "lon": "",
-                    "boundingbox": ""
                     })
     return cities_infos
     
