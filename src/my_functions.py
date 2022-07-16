c8218274d1b773402093873cb7281fe5649ba6f1
hmrani <hicham.mrani@outlook.fr>
2022-07-16 21:23:21 +0200

    update my_functions file

--- src/my_functions.py
+++ src/my_functions.py
@@ -2,6 +2,11 @@ from pathlib import Path
 import pandas as pd
 import json
 
+def export_csv(data:list, rel_path:str, file_name:str):
+    filepath = Path(rel_path + '/' + file_name)
+    df = pd.DataFrame(data=data)
+    df.to_csv(filepath)
+
 def export_json(dictionary:dict, rel_path:str, file_name:str):
     json_object = json.dumps(dictionary, indent = 4)
     with open(f"{rel_path}{file_name}.json", "w") as outfile:
